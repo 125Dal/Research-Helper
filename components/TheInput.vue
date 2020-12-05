6cb11e52eaceca0c7e1cd152c30acd44670d934a
125Dal <hhmyimn@hotmail.com>
2020-12-05 19:02:53 +0900

    :sparkles: 入力コンポーネント追加

---
+++ components/TheInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input :value="value" class="border border-gray-500 px-3 py-3" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'TheInput',
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  }
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,7 +68,7 @@ const configuration: Configuration = {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/scss/_base.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['~/plugins/directive/testid'],
