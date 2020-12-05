798f98108afa7d78cbcaebe32320f350f466dde3
125Dal <hhmyimn@hotmail.com>
2020-12-06 02:34:57 +0900

    :sparkles: サイドバーに表示するコンテンツの一部を作成

---
+++ components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="overflow-hidden border border-gray-500 rounded-xl shadow-xl"
+    data-testId="SideBar"
+  >
+    <header class="px-8 py-6 border-b text-4xl" data-testId="SideBarTitle">
+      {{ title }}
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'SideBar',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
---
+++ components/TagItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li
+    class="flex justify-between items-center break-all cursor-pointer mb-8 last:mb-0"
+    data-testId="TagItem"
+    @click="tagClicked"
+  >
+    <span data-testId="TagName">{{ tag.name }}</span>
+    <span
+      class="flex-shrink-0 w-13 inline-block ml-4 text-right"
+      data-testId="TagCount"
+    >
+      {{ tag.count }}
+    </span>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+import { Tag } from '~/types/valueObject/Tag'
+
+export default defineComponent({
+  name: 'TagItem',
+  props: {
+    tag: {
+      type: Object as PropType<Tag>,
+      required: true
+    }
+  },
+  setup({ tag }, { emit }) {
+    const tagClicked = () => emit('tag-clicked', tag.name)
+
+    return { tagClicked }
+  }
+})
+</script>
